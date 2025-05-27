33a7b6e5da6e55f1cf07c9b031ade9a7551b4333
MariamaCherifAidara <balbishaidara@gmail.com>
2025-05-27 14:17:55 +0200

    added open feign

--- patient/src/main/java/com/example/Patient/controller/PatientController.java
+++ patient/src/main/java/com/example/Patient/controller/PatientController.java
@@ -1,7 +1,9 @@
 package com.example.Patient.controller;
 
+import com.example.Patient.dto.request.AppuntamentoRequest;
 import com.example.Patient.dto.request.FiltroPatientRequest;
 import com.example.Patient.dto.request.PatientRequest;
+import com.example.Patient.dto.response.AppuntamentoResponse1;
 import com.example.Patient.dto.response.PatientResponse;
 import com.example.Patient.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,18 +21,32 @@ public class PatientController {
     @Autowired
     PatientService patientService;
 
+    @PostMapping("/creaAppuntamento")
+    public ResponseEntity creaAppuntamento(@RequestBody AppuntamentoRequest appuntamentoRequest) {
+        AppuntamentoResponse1 appuntamentoResponse1=patientService.creaAppuntamento(appuntamentoRequest);
+        if (appuntamentoResponse1 != null) {
+            return ResponseEntity.ok("il paziente " + appuntamentoResponse1.getNomePaziente() + " " + appuntamentoResponse1.getCognomePaziente() +
+                    " nato il " + appuntamentoResponse1.getData_nascitaPaziente().toString()+ "con il seguente nuumero di telefono "+ appuntamentoResponse1.getNumero_telefonoPaziente()+
+                    "con il seguente email: "+ appuntamentoResponse1.getEmailPaziente() + " ha prenotato con il dottore " + appuntamentoResponse1.getNomeDoctor()
+            +" " + appuntamentoResponse1.getCognomeDoctor() + " sprcializzato in " + appuntamentoResponse1.getSpecializzazione() + " la data dell'appuntamento è fissato per il "+ appuntamentoResponse1.getDataAppuntamento() +
+                    " a " + appuntamentoResponse1.getLuogoAppuntamento());
 
-    @PostMapping("/ricercaPatientFiltro")
-    public ResponseEntity ricercaPatient(@RequestBody FiltroPatientRequest filtroPatientRequest) {
-
-        List<PatientResponse> patientResponseList=patientService.ricercaPatient(filtroPatientRequest);
-        if (patientResponseList != null) {
-           return ResponseEntity.ok(patientResponseList);
         }
         return ResponseEntity.notFound().build();
     }
 
-    @PostMapping(value = "/savePatient",consumes = MediaType.APPLICATION_JSON_VALUE,
+
+    @PostMapping("/ricercaPatientFiltro")
+    public ResponseEntity ricercaPatient(@RequestBody FiltroPatientRequest filtroPatientRequest) {
+
+        List<PatientResponse> patientResponseList = patientService.ricercaPatient(filtroPatientRequest);
+        if (patientResponseList != null) {
+            return ResponseEntity.ok(patientResponseList);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping(value = "/savePatient", consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<PatientResponse> savePatient(@RequestBody PatientRequest patientRequest) {
         PatientResponse patientResponse = patientService.savePatient(patientRequest);
@@ -41,7 +57,7 @@ public class PatientController {
     }
 
     @PutMapping("/updatePatient/{idPatient}")
-    public  ResponseEntity<PatientResponse>  updatePatient(@RequestBody PatientRequest patientRequest, @PathVariable Long idPatient) {
+    public ResponseEntity<PatientResponse> updatePatient(@RequestBody PatientRequest patientRequest, @PathVariable Long idPatient) {
 
         PatientResponse patientResponse = patientService.updatePatient(patientRequest, idPatient);
         if (patientResponse != null) {
@@ -60,7 +76,7 @@ public class PatientController {
     }
 
     @GetMapping("/getPatient/{idPatient}")
-    public  ResponseEntity<PatientResponse>  getPatient(@PathVariable Long idPatient) {
+    public ResponseEntity<PatientResponse> getPatient(@PathVariable Long idPatient) {
         PatientResponse patientResponse = patientService.getPatient(idPatient);
         if (patientResponse != null) {
             return ResponseEntity.ok(patientResponse);
