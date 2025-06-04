297dcb4fd3746fdb11b26da97b60f0114b368e6d
nabilAesys <nabil.ezzaki@aesys.tech>
2025-06-04 09:44:22 +0200

    added resiliance4j and circuitbreaker

---
+++ api-gateway-cloud/src/main/java/com/example/api_gateway/controller/FallbackController.java
@@ -0,0 +1,22 @@
+package com.example.api_gateway.controller;
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackController {
+
+    @GetMapping("/doctorFallBack")
+    public String doctorFallBack() {
+        return "Doctor Service is down!";
+    }
+
+    @PostMapping("/appuntamentoFallBack")
+    public String appuntamentoFallBack() {
+        return "Appuntamento Service is down!";
+    }
+
+}
